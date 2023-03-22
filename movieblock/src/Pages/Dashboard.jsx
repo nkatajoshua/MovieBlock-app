b05c36664c044d57af02c27f658f9e546655e8ee
nkatajoshua <93196162+nkatajoshua@users.noreply.github.com>
2023-03-22 15:30:46 +0300

    Foundation

--- movieblock/src/Pages/Dashboard.jsx
+++ movieblock/src/Pages/Dashboard.jsx
@@ -1,8 +1,16 @@
 import React from "react"
+import './Dashboard.css';
 
 function Dashboard(){
     return(
-        <h1>Dashboard</h1>
+       <nav>
+        <ul>
+            <li><a>Dashboard</a></li>
+        </ul>
+       </nav>
+
+       {/*Create a seacrh form with a submit button*/}
+       
     )
 }
 
