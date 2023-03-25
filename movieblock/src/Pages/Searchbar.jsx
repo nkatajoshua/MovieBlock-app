610f5e2acd72539569b345ab66b7863940b16185
nkatajoshua <93196162+nkatajoshua@users.noreply.github.com>
2023-03-25 17:33:13 +0300

    Update Searchbar.jsx

--- movieblock/src/Pages/Searchbar.jsx
+++ movieblock/src/Pages/Searchbar.jsx
@@ -4,39 +4,29 @@ import axios from 'axios';
 
 
 function SerachBar(props){
-    
-    const axios = require("axios");
 
-    const options = {
-      method: 'GET',
-      url: 'https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup',
-      params: {term: 'bojack', country: 'uk'},
-      headers: {
-        'X-RapidAPI-Key': 'bb0ff7c2abmsh96b734ed2f509b6p1425a7jsn434f2ec8c6c8',
-        'X-RapidAPI-Host': 'utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com'
-      }
-    };
-    
-    axios.request(options).then(function (response) {
-        console.log(response.data);
-    }).catch(function (error) {
-        console.error(error);
-    });
-    
-    
-    const [searchTerm, setSearchTerm] = useState('');
+   const[nameList, setNameList] = useState([])
+   const [searchTerm, handleSubmit] = useState ('')
 
-    function handleChange(event){
-        setSearchTerm(event.target.value);
-    }
-    function handleSubmit(event){
-        event.preventDefault();
-        props.onSearch(searchTerm);
-    }
+   useEffect(()=>{
+    axios.get('https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup?limit=10')
+    .then((response)=>{setNameList(response.data.results)})
+   },[])
 
     return(
         <form onSubmit={handleSubmit} className='formdesign'>
-            <input type='text' placeholder='What do you want to watch....' value={searchTerm} onChange={handleChange}/>
+            <input type='text' placeholder='What do you want to watch....' value={searchTerm} onChange={(e)=>handleSubmit(e.target.value)}/>
+            {nameList.filter((item)=>{
+                if (searchTerm===""){
+                   return item 
+                }
+                else if(item.name.toLowerCase().includes(searchTerm.toLowerCase())){
+                    return item
+                }
+            })
+            .map((item)=>{
+            return <h1>{item.name}</h1>
+            })}
             <button type='submit'>Search</button>
         </form>
     );
