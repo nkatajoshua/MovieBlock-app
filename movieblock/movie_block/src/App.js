ac0dbec4f18e8ed59632ea50ede1483b9cc10578
nkatajoshua <93196162+nkatajoshua@users.noreply.github.com>
2023-04-08 21:46:30 +0300

    Update App.js

--- movieblock/movie_block/src/App.js
+++ movieblock/movie_block/src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import './App.css';
 import Home from './Pages/Home';
-import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <>
-    <Navbar/>
     <Home/>
     </>
   );
