fe5e73416bf42240d55d584f9201dbc8aa443a6c
nkatajoshua <93196162+nkatajoshua@users.noreply.github.com>
2023-04-13 22:59:46 +0300

    position

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM node:14
-WORKDIR /movieblock/movie_block/src
-COPY /movieblock/movie_block .
-RUN npm install
-ENV NODE_ENV production
-EXPOSE 3001
-CMD [ "npm", "start" ]
--- src/Pages/Tvshows.js
+++ src/Pages/Tvshows.js
@@ -2,18 +2,27 @@ import React, { useEffect, useState } from 'react';
 
 const Tvshows = () => {
   const [trendingTvShows, setTrendingTvShows] = useState([]);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     const fetchTrendingTvShows = async () => {
-      const response = await fetch(
-        'https://api.themoviedb.org/3/trending/tv/week?api_key=311c30cde07e03c2566a3d8d8409c3b8'
-      );
-      const data = await response.json();
-      setTrendingTvShows(data.results);
+      try {
+        const response = await fetch(
+          'https://api.themoviedb.org/3/trending/tv/week?api_key=311c30cde07e03c2566a3d8d8409c3b8'
+        );
+        const data = await response.json();
+        setTrendingTvShows(data.results);
+      } catch (error) {
+        setError(true);
+      }
     };
 
     fetchTrendingTvShows();
   }, []);
+
+  if (error) {
+    return <div>Something went wrong...</div>;
+  }
 
   return (
     <>
